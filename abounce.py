2c8347e97a95bcde2dae56b5f87451447d8262cd
Zach Stednick <zstednic@fhcrc.org>
2014-07-23 15:43:45 -0700

    initial setup

---
+++ abounce.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'abounce.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('ABOUNCE_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select id, next from main order by id desc')
+    entries = cur.fetchall()
+    return render_template('main.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run()
---
+++ schema.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS main;
+CREATE TABLE main(
+  id Integer PRIMARY KEY AUTOINCREMENT,
+  next date,
+  comments text);
